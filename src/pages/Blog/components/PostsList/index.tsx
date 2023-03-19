0f1381f37e05adac6ec0443a0b99dce4ca2b048f
Renato <renatomarquesteles@gmail.com>
2023-03-19 18:12:18 -0300

    feat: list posts from github

--- src/pages/Blog/components/PostsList/index.tsx
+++ src/pages/Blog/components/PostsList/index.tsx
@@ -1,19 +1,54 @@
+import { useEffect, useState } from 'react';
+
+import * as dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+import { api } from '../../../../lib/axios';
+
 import { Container, PostContainer, Title } from './styles';
 
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  createdAt: string;
+}
+
+dayjs.extend(relativeTime);
+
 export const PostsList = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const fetchPosts = async () => {
+    const { data } = await api.get(
+      '/repos/renatomarquesteles/ignite-03-github-blog/issues'
+    );
+
+    const formattedPosts = data.map((post: any) => {
+      return {
+        id: post.id,
+        title: post.title,
+        body: post.body,
+        createdAt: new Date(post.created_at),
+      };
+    });
+
+    setPosts(formattedPosts);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <Container>
-      {Array.from({ length: 6 }).map((_, index) => (
-        <PostContainer key={index}>
+      {posts.map((post) => (
+        <PostContainer key={post.id}>
           <Title>
-            <h1>JavaScript data types and data structures</h1>
-            <span>1 day ago</span>
+            <h1>{post.title}</h1>
+            <span>{dayjs(post.createdAt).fromNow()}</span>
           </Title>
-          <p>
-            Programming languages all have built-in data structures, but these
-            often differ from one language to another. This article attempts to
-            list the built-in data structures available in...
-          </p>
+          <p>{post.body}</p>
         </PostContainer>
       ))}
     </Container>
